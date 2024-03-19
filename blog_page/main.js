5c892ee28ae328c5d3a332f7f15432cbbbc6b6df
Rakshak <rakshakchoudhary14@gmail.com>
2024-03-19 14:05:18 +0530

    Add all files

---
+++ blog_page/main.js
@@ -0,0 +1,48 @@
+// Read blog 1
+const readbtn = document.querySelector("#read-more-btn");
+const readmore = document.querySelector("#read-more");
+
+readbtn.addEventListener("click", function () {
+  readmore.style.display = "block";
+  this.style.display = "none";
+});
+
+// read blog 2
+
+const readbtn1 = document.querySelector("#read-more-btn1");
+const readmore1 = document.querySelector("#read-more1");
+
+readbtn1.addEventListener("click", function () {
+  readmore1.style.display = "block";
+  this.style.display = "none";
+});
+
+// read blog 3
+
+const readbtn2 = document.querySelector("#read-more-btn2");
+const readmore2 = document.querySelector("#read-more2");
+
+readbtn2.addEventListener("click", function () {
+  readmore2.style.display = "block";
+  this.style.display = "none";
+});
+
+// read blog 4
+
+const readbtn3 = document.querySelector("#read-more-btn3");
+const readmore3 = document.querySelector("#read-more3");
+
+readbtn3.addEventListener("click", function () {
+  readmore3.style.display = "block";
+  this.style.display = "none";
+});
+
+// read blog 3
+
+const readbtn4 = document.querySelector("#read-more-btn4");
+const readmore4 = document.querySelector("#read-more4");
+
+readbtn4.addEventListener("click", function () {
+  readmore4.style.display = "block";
+  this.style.display = "none";
+});
